eb175123b123122a2dab69dc9441a7dd896bed9e
KARANI JASWANTH <karani.jaswanth@publicissapient.com>
2020-03-03 11:54:37 +0530

    asasd

---
+++ splitwise/splitwise/src/createGroup.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+
+export default class createGroup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      group: {
+        name: "",
+        friends: [],
+        createdBy: 1
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <button
+            type="button"
+            className="btn btn-info btn-lg"
+            data-toggle="modal"
+            data-target="#createGroup"
+          >
+            Create Group
+          </button>
+        </div>
+        <div className="modal fade" id="createGroup">
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Create Group</h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <form>
+                  <div class="form-group">
+                      <label class="control-label" for="groupName">
+                        Group Name
+                      </label>
+                      <input
+                        class="form-control"
+                        id="groupName"
+                        name="name"
+                        type="text"
+                        placeholder="Group Name"
+                       
+                      />
+                  </div>
+                </form>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-primary">
+                  Create
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
